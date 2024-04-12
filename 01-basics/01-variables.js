f7ed67b08c46d601af2b5f53e91828df741f6b9b
atuljag <166813245+atuljag@users.noreply.github.com>
2024-04-12 12:12:22 +0000

    variables and datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,9 @@
+const accountId = 123;
+let emailId = "test@google.com";
+var password = "12345";
+accountCity = "pune";
+
+console.log(accountId);
+console.table([emailId,password,accountCity]);
+
+/* Prefer not to use VAR as its having issue with block scope and functional scope*/
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as a newer version
+
+let name = "Atul"
+let age = 27
+let isLoggedin = true
+
+//datatypes
+//number , string , boolean , null , undefined , symbol
